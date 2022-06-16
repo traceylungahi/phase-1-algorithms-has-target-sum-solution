6bc71db5a9400c3a3840be32f69da3f52da548b8
traceymmbonelungahi <t.lungahi@gmail.com>
2022-06-16 20:51:57 +0300

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = {};
+  for (const number of array) {
+    const complement = target - number; 
+    if (seenNumbers[complement]) return true;
+    seenNumbers[number] = true;
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  write a function that takes an array and a target interger as arguments
+  if sum of pair of numbersin array add up to target interger:
+    return true
+    else
+    return false
 */
 
 /*
   Add written explanation of your solution here
+  Write a function called hasTargetSum, it should receive two arguments that are: an array of integers and a target integer. The function is supposed to return true if any pairs of numbers in the array adds up to the target number.
 */
 
 // You can run `node index.js` to view these console logs
